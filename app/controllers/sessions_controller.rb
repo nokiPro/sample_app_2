c1483d46094e247121485c95b7e4f22b636b4cc5
d.ogawa <noki.g2000@gmail.com>
2021-12-26 02:50:03 +0000

    8.1.5

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,9 +5,8 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      # ユーザーログイン後にユーザー情報のページにリダイレクトする
     else
-      # エラーメッセージを作成する
+      flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
     end
   end
