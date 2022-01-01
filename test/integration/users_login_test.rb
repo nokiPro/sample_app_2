777840b865abe6611f6372b65b8249a8609786f7
d.ogawa <noki.g2000@gmail.com>
2022-01-01 09:21:45 +0000

    9.3.1

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -6,6 +6,20 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     @user = users(:michael)
   end
   
+  test "login with remembering" do
+    log_in_as(@user, remember_me: '1')
+    assert_equal cookies['remember_token'], assigns(:user).remember_token
+  end
+
+  test "login without remembering" do
+    # cookieを保存してログイン
+    log_in_as(@user, remember_me: '1')
+    delete logout_path
+    # cookieを削除してログイン
+    log_in_as(@user, remember_me: '0')
+    assert_empty cookies[:remember_token]
+  end
+  
   test "login with valid email/invalid password" do
     get login_path
     assert_template 'sessions/new'
